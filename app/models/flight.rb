4a9e28b5c44877cc0cc1cae45c12ffe816cc4aba
Cheong <xxlsxx1955@gmail.com>
2023-04-02 21:33:10 +0800

    Include Flight model validations

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,7 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
+
+  validates :departure_airport_id, :arrival_airport_id, presence: true
+  validates :start, :duration, presence: true
 end
