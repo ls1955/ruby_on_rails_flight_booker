9cd9d96e506f2294a8911ede2fa2225c7f4cdcbd
Cheong <xxlsxx1955@gmail.com>
2023-04-02 21:17:56 +0800

    Include Flight model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_02_123838) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_02_130017) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.string "name"
@@ -18,4 +18,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_02_123838) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "flights", force: :cascade do |t|
+    t.integer "departure_airport_id", null: false
+    t.integer "arrival_airport_id", null: false
+    t.datetime "start"
+    t.string "duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["arrival_airport_id"], name: "index_flights_on_arrival_airport_id"
+    t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
+  end
+
+  add_foreign_key "flights", "airports", column: "arrival_airport_id"
+  add_foreign_key "flights", "airports", column: "departure_airport_id"
 end
