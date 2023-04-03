731cf03ad92081f122137ee2d1049f5c87ce69e4
Cheong <xxlsxx1955@gmail.com>
2023-04-03 20:37:02 +0800

    Establish associations
    - Establish 1:n associations between Flight and Booking
    - Establish 1:n associations between Passenger and Booking
    - Establish m:n associations between Flight and Passenger

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,6 +2,9 @@ class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
 
+  has_many :bookings
+  has_many :passengers, through: :bookings
+
   validates :departure_airport_id, :arrival_airport_id, presence: true
   validates :depart, :duration, presence: true
 
