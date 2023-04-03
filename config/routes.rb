80660697b590ca263afb5928723ecea2b24384df
Cheong <xxlsxx1955@gmail.com>
2023-04-03 15:14:19 +0800

    Include BookingsController and #new view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :flights, only: :index
+  resources :bookings, only: :new
 
   root "flights#index"
 end
