fe3287a0d5361b75a771087bae41de1b51c6d94e
Cheong <xxlsxx1955@gmail.com>
2023-04-03 15:55:02 +0800

    Fix bug
    - Validate column depart instead of start

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,7 +3,7 @@ class Flight < ApplicationRecord
   belongs_to :arrival_airport, class_name: "Airport"
 
   validates :departure_airport_id, :arrival_airport_id, presence: true
-  validates :start, :duration, presence: true
+  validates :depart, :duration, presence: true
 
   scope :same_date, ->(depart) { where depart: depart.beginning_of_day..depart.end_of_day }
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_03_022153) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_03_074343) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
