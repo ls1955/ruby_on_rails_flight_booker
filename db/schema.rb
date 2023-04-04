0ce3d264dbc63abe3a48e1f61efba467d668064d
Cheong <xxlsxx1955@gmail.com>
2023-04-04 09:32:51 +0800

    Add passengers' booking ref

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_04_012852) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_04_013026) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.string "name"
@@ -41,9 +41,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_04_012852) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "booking_id", null: false
+    t.index ["booking_id"], name: "index_passengers_on_booking_id"
   end
 
   add_foreign_key "bookings", "flights"
   add_foreign_key "flights", "airports", column: "arrival_airport_id"
   add_foreign_key "flights", "airports", column: "departure_airport_id"
+  add_foreign_key "passengers", "bookings"
 end
