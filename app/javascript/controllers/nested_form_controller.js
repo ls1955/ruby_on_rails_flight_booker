aa77d2792331f5eaba3e2b461cac5ea3cd037888
Cheong <xxlsxx1955@gmail.com>
2023-04-19 16:45:17 +0800

    Allow deletion of nested passengers' fields

--- app/javascript/controllers/nested_form_controller.js
+++ app/javascript/controllers/nested_form_controller.js
@@ -9,4 +9,12 @@ export default class extends Controller {
         let content = this.templateTarget.innerHTML.replace(/TEMPLATE_RECORD/g, Math.floor(Math.random() * 20))
         this.addItemTarget.insertAdjacentHTML("beforebegin", content)
     }
+
+    deleteAssociation(event) {
+        event.preventDefault()
+
+        let item = event.target.closest(".nested-fields")
+        item.querySelector("input[name*='_destroy']").value = 1
+        item.hidden = true
+    }
 }
