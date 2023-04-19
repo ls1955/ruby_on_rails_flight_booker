3134eb205cc461141714feb404dcd7035a756a00
Cheong <xxlsxx1955@gmail.com>
2023-04-19 16:22:29 +0800

    Allow passenger to be added dynamically

--- app/javascript/controllers/nested_form_controller.js
+++ app/javascript/controllers/nested_form_controller.js
@@ -1,5 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    deleteAssociation() {}
+    static targets = ["template", "addItem"]
+
+    addAssociation(event) {
+        event.preventDefault()
+
+        let content = this.templateTarget.innerHTML.replace(/TEMPLATE_RECORD/g, Math.floor(Math.random() * 20))
+        this.addItemTarget.insertAdjacentHTML("beforebegin", content)
+    }
 }
