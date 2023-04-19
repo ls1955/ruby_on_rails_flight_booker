053cf91cf2f34d60ab56a95d0b82bcb7b68539f4
Cheong <xxlsxx1955@gmail.com>
2023-04-19 16:46:34 +0800

    Prevent deletion of last passenger's fields

--- app/javascript/controllers/nested_form_controller.js
+++ app/javascript/controllers/nested_form_controller.js
@@ -13,8 +13,15 @@ export default class extends Controller {
     deleteAssociation(event) {
         event.preventDefault()
 
+        let passengers = document.querySelectorAll(".nested-fields:not(.destroyed)")
+        if (passengers.length === 1) {
+            window.alert("Must have at least 1 passenger")
+            return
+        }
+
         let item = event.target.closest(".nested-fields")
         item.querySelector("input[name*='_destroy']").value = 1
+        item.classList.add("destroyed")
         item.hidden = true
     }
 }
