5b29dd1230aee31f3d5ed85c833411359dcd6a96
Cheong <xxlsxx1955@gmail.com>
2023-04-21 20:42:02 +0800

    Implement PassengerMailer#confirmation_email

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,10 @@
 class PassengerMailer < ApplicationMailer
+  default from: "donotreply@odin.com"
+
+  def confirmation_email
+    @passenger = params[:passenger]
+    @flight = @passenger.booking.flight
+
+    mail to: @passenger.email, subject: "Odin - Confirm your booking"
+  end
 end
--- test/mailers/previews/passenger_mailer_preview.rb
+++ test/mailers/previews/passenger_mailer_preview.rb
@@ -1,4 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/passenger_mailer
 class PassengerMailerPreview < ActionMailer::Preview
+  def confirmation_email
+    @passenger = Passenger.first
 
+    PassengerMailer.with(passenger: @passenger).confirmation_email
+  end
 end
